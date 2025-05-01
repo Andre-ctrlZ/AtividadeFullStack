0ef346c31cff606d47c834d2b301b8437d0f55fc
Andre-ctrlZ <luiace2003@gmail.com>
2025-04-30 21:42:01 -0300

    SAVE, não exibe cadastrar

---
+++ AtividadeFullStack/AtividadeFullStack/src/main/java/com/example/AtividadeFullStack/exceptions/EmailJaCadastradoException.java
@@ -0,0 +1,7 @@
+package com.example.AtividadeFullStack.exceptions;
+
+public class EmailJaCadastradoException extends RuntimeException {
+    public EmailJaCadastradoException(String message) {
+        super(message);
+    }
+}
